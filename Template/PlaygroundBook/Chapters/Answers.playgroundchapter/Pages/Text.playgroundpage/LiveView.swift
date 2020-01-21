69934cf07e97c67b89e938e6e395bc119e949641
iTBoyer <itboyer@qq.com>
2020-01-21 19:25:18 +0800

    update

--- Template/PlaygroundBook/Chapters/Answers.playgroundchapter/Pages/Text.playgroundpage/LiveView.swift
+++ Template/PlaygroundBook/Chapters/Answers.playgroundchapter/Pages/Text.playgroundpage/LiveView.swift
@@ -4,6 +4,10 @@
 //  Copyright © 2016-2019 Apple Inc. All rights reserved.
 //
 
-import Book
-
+//import Book
+//
 setupLiveView()
+//import PlaygroundSupport
+
+// Instantiate a new instance of the live view from the book's auxiliary sources and pass it to PlaygroundSupport.
+//PlaygroundPage.current.liveView = instantiateLiveView()
