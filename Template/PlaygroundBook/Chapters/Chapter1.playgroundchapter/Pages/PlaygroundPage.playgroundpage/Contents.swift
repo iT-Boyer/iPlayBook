c039ad34ba66a5c248319d937cdc4465c4ffd86d
iTBoyer <itboyer@qq.com>
2020-01-21 12:32:15 +0800

    update

--- Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
+++ Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/Contents.swift
@@ -6,4 +6,14 @@
 //  The Swift file containing the source code edited by the user of this playground book.
 //
 //#-end-hidden-code
-let str = "Hello, playground"
+// 1. 请求一个日期
+show("你的生日是哪天？")
+let birthdate = askForDate("生日")
+
+// 2. 请求一个数字
+show("你最喜欢的数字是什么？")
+let number = askForNumber("数字")
+
+// 3. 请求从一组选项中选出一个
+show("你最喜欢的颜色是什么？")
+let color = askForChoice("颜色", options: ["蓝色", "绿色", "橙色", "紫色", "红色", "黄色"])
